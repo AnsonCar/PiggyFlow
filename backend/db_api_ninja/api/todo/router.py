9fb3c17615f3c78440a5501db0390f90ac314c11
AnsonCar <anson1tsang@gmail.com>
2024-07-20 17:18:40 +0800

    update todo api service file

--- backend/db_api_ninja/api/todo/router.py
+++ backend/db_api_ninja/api/todo/router.py
@@ -1,9 +1,17 @@
 import uuid
 from ninja import Router
-from django.shortcuts import get_object_or_404
 from api.utils.download import download_csv
+
 from .models import ToDo
 from .schema import ToDoIn, ToDoList, ToDoOut, ToDoDone
+from .service import (
+    get_todos_service,
+    get_todo_service,
+    update_todo_service,
+    create_todo_service,
+    delete_todo_service,
+    update_todo_done_service
+)
 from ninja_jwt.authentication import JWTAuth
 
 MyModel = ToDo
@@ -16,50 +24,37 @@ router = Router(tags=["todo"])
 
 @router.get("", auth=JWTAuth(), response=ModelList)
 def get_todos(request):
-    data = MyModel.objects.all()
+    data = get_todos_service()
     return ModelList(data=data)
 
 
 @router.get("/{uuid}", auth=JWTAuth(), response=ModeOut)
 def get_todo(request, uuid: uuid.UUID):
-    data = get_object_or_404(MyModel, uuid=uuid)
-    return data
+    return get_todo_service(uuid)
 
 
 @router.post("", auth=JWTAuth())
 def create_todo(request, payload: ModelIn):
     addData = payload.dict()
     addData["user_uuid"] = request.user.uuid
-    data = MyModel.objects.create(**addData)
-    return {"id": data.id}
+    return create_todo_service(addData)
 
 
 @router.put("/{uuid}", auth=JWTAuth())
 def update_todo(request, uuid: uuid.UUID, payload: ModelIn):
-    data = get_object_or_404(MyModel, uuid=uuid)
-    for attr, value in payload.dict().items():
-        setattr(data, attr, value)
-    data.save()
-    return {"success": True}
+    return update_todo_service(uuid, payload)
 
 
 @router.delete("/{uuid}", auth=JWTAuth())
 def delete_todo(request, uuid: uuid.UUID):
-    data = get_object_or_404(MyModel, uuid=uuid)
-    data.delete()
-    return {"success": True}
+    return delete_todo_service(uuid)
 
 
 @router.put("/done/{uuid}", auth=JWTAuth())
 def update_todo_done(request, uuid: uuid.UUID, payload: ToDoDone):
-    data = get_object_or_404(MyModel, uuid=uuid)
-    for attr, value in payload.dict().items():
-        if attr == "done":
-            setattr(data, attr, value)
-    data.save()
-    return {"success": True}
+    return update_todo_done_service(uuid, payload)
 
 
 @router.post("/download/csv", auth=JWTAuth())
 def download_todo_csv(request):
-    return download_csv(MyModel, 'todo')
+    return download_csv( MyModel, "todo" )
