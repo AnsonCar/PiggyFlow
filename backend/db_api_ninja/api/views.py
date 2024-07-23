6380a7ae6e9a0682f6b254a1bf00b79e01c3f46c
AnsonCar <anson1tsang@gmail.com>
2024-07-24 00:51:45 +0800

    change api.add_router hide translate router

--- backend/db_api_ninja/api/views.py
+++ backend/db_api_ninja/api/views.py
@@ -7,10 +7,9 @@ api.register_controllers(NinjaJWTDefaultController)
 from .user.router import router as userRouter
 from .user_group.router import router as groupRouter
 
-# from .auth.router import authRouter as authRouter
 from .transaction.router import router as transactionRouter
 from .todo.router import router as todoRouter
-from .translate.router import router as translateRouter
+# from .translate.router import router as translateRouter
 
 api.add_router("/user", userRouter)
 # api.add_router("/auth", authRouter)
@@ -18,4 +17,4 @@ api.add_router("/group", groupRouter)
 
 api.add_router("/transaction", transactionRouter)
 api.add_router("/todo", todoRouter)
-api.add_router("/translate", translateRouter)
+# api.add_router("/translate", translateRouter)
