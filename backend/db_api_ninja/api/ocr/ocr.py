ace32b288f7ec0753d7c8e6b8bec77ff1ee3087d
Rex <shing1124@gmail.com>
2024-07-24 20:08:59 +0800

    rex

--- backend/db_api_ninja/api/ocr/ocr.py
+++ backend/db_api_ninja/api/ocr/ocr.py
@@ -1,4 +1,4 @@
-from paddleocr import PaddleOCR, draw_ocr
+"""from paddleocr import PaddleOCR, draw_ocr
 
 # Paddleocr目前支持的多语言语种可以通过修改lang参数进行切换
 # 例如`ch`, `en`, `fr`, `german`, `korean`, `japan`
@@ -19,4 +19,15 @@ txts = [line[1][0] for line in result]
 scores = [line[1][1] for line in result]
 im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
 im_show = Image.fromarray(im_show)
-im_show.save('result.jpg')
+im_show.save('result.jpg')"""
+
+import os
+#os.chdir('/content/drive/MyDrive/Colab Notebooks')  # Colab 換路徑使用
+
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+img = Image.open(r'C:\Users\susu\PiggyFlow\backend\db_api_ninja\api\imgs\1.jpg')
+#img = Image.open('./11.jpg')
+text = pytesseract.image_to_string(img, lang='eng')
+print(text)
