e4cedbb31410b64147698c62a76352ff641de768
AnsonCar <anson1tsang@gmail.com>
2024-08-10 00:21:19 +0800

    orjson

--- backend/pgsql_django_ninja_api/db_api_ninja/api/views.py
+++ backend/pgsql_django_ninja_api/db_api_ninja/api/views.py
@@ -1,24 +1,34 @@
-from ninja_jwt.controller import NinjaJWTDefaultController
-from ninja_extra import NinjaExtraAPI
 import os
+import orjson
+from ninja_extra import NinjaExtraAPI
+from ninja.parser import Parser
+from ninja_jwt.controller import NinjaJWTDefaultController
+
+# from .translate.router import router as translateRouter
+from .todo.router import router as todoRouter
+from .transaction.router import router as transactionRouter
+from .user_group.router import router as groupRouter
+from .user.router import router as userRouter
+
+
+class ORJSONParser(Parser):
+    def parse_body(self, request):
+        return orjson.loads(request.body)
+
+
 MODE = os.getenv('DJANGO_DEPLOY', 'Null')
 NAME = f"DataBase NinjaExtra API | {MODE.capitalize()} "
 
-api = NinjaExtraAPI(app_name=NAME,title=NAME)
+api = NinjaExtraAPI(app_name=NAME, title=NAME, parser=ORJSONParser())
 api.register_controllers(NinjaJWTDefaultController)
 
-from .user.router import router as userRouter
-from .user_group.router import router as groupRouter
-
 # from .auth.router import authRouter as authRouter
-from .transaction.router import router as transactionRouter
-from .todo.router import router as todoRouter
-from .translate.router import router as translateRouter
 
 # auth
 api.add_router("/user", userRouter)
 api.add_router("/group", groupRouter)
-
+# 
 api.add_router("/transaction", transactionRouter)
 api.add_router("/todo", todoRouter)
-api.add_router("/translate", translateRouter)
+#
+# api.add_router("/translate", translateRouter)
