68271a172dd75c156a6888572959c132a9c46eb6
AnsonCar <anson1tsang@gmail.com>
2024-08-10 02:22:43 +0800

    add back orjson

--- backend/pgsql_django_ninja_api/db_api_ninja/api/views.py
+++ backend/pgsql_django_ninja_api/db_api_ninja/api/views.py
@@ -4,22 +4,21 @@ from ninja_extra import NinjaExtraAPI
 from ninja.parser import Parser
 from ninja_jwt.controller import NinjaJWTDefaultController
 
-# from .translate.router import router as translateRouter
+from .translate.router import router as translateRouter
 from .todo.router import router as todoRouter
 from .transaction.router import router as transactionRouter
 from .user_group.router import router as groupRouter
 from .user.router import router as userRouter
 
 
-# class ORJSONParser(Parser):
-#     def parse_body(self, request):
-#         return orjson.loads(request.body)
+class ORJSONParser(Parser):
+    def parse_body(self, request):
+        return orjson.loads(request.body)
 
 
 MODE = os.getenv('DJANGO_DEPLOY', 'Null')
 NAME = f"DataBase NinjaExtra API | {MODE.capitalize()} "
-# , parser=ORJSONParser()
-api = NinjaExtraAPI(app_name=NAME, title=NAME)
+api = NinjaExtraAPI(app_name=NAME, title=NAME, parser=ORJSONParser())
 api.register_controllers(NinjaJWTDefaultController)
 
 # from .auth.router import authRouter as authRouter
