a83aaaa36c08e00480aa82a802a6fa39812f51d5
AnsonCar <anson1tsang@gmail.com>
2024-08-10 00:21:48 +0800

    async todo

--- backend/pgsql_django_ninja_api/db_api_ninja/api/todo/router.py
+++ backend/pgsql_django_ninja_api/db_api_ninja/api/todo/router.py
@@ -23,38 +23,38 @@ router = Router(tags=["todo"])
 
 
 @router.get("", auth=JWTAuth(), response=ModelList)
-def get_todos(request):
-    data = get_todos_service()
+async def get_todos(request):
+    data = await get_todos_service()
     return ModelList(data=data)
 
 
 @router.get("/{uuid}", auth=JWTAuth(), response=ModeOut)
-def get_todo(request, uuid: uuid.UUID):
-    return get_todo_service(uuid)
+async def get_todo(request, uuid: uuid.UUID):
+    return await get_todo_service(uuid)
 
 
 @router.post("", auth=JWTAuth())
-def create_todo(request, payload: ModelIn):
-    addData = payload.dict()
+async def create_todo(request, payload: ModelIn):
+    addData = await payload.dict()
     addData["user_uuid"] = request.user.uuid
-    return create_todo_service(addData)
+    return await create_todo_service(addData)
 
 
 @router.put("/{uuid}", auth=JWTAuth())
-def update_todo(request, uuid: uuid.UUID, payload: ModelIn):
-    return update_todo_service(uuid, payload)
+async def update_todo(request, uuid: uuid.UUID, payload: ModelIn):
+    return await update_todo_service(uuid, payload)
 
 
 @router.put("/done/{uuid}", auth=JWTAuth())
-def update_todo_done(request, uuid: uuid.UUID, payload: ToDoDone):
-    return update_todo_done_service(uuid, payload)
+async def update_todo_done(request, uuid: uuid.UUID, payload: ToDoDone):
+    return await update_todo_done_service(uuid, payload)
 
 
 @router.delete("/{uuid}", auth=JWTAuth())
-def delete_todo(request, uuid: uuid.UUID):
-    return delete_todo_service(uuid)
+async def delete_todo(request, uuid: uuid.UUID):
+    return await delete_todo_service(uuid)
 
 
 @router.post("/download/csv", auth=JWTAuth())
-def download_todo_csv(request):
-    return download_csv(MyModel, "todo")
+async def download_todo_csv(request):
+    return await download_csv(MyModel, "todo")
--- backend/pgsql_django_ninja_api/db_api_ninja/api/todo/service.py
+++ backend/pgsql_django_ninja_api/db_api_ninja/api/todo/service.py
@@ -5,30 +5,30 @@ from django.shortcuts import get_object_or_404
 MyModel = ToDo
 
 
-def get_todos_service():
-    return MyModel.objects.all()
+async def get_todos_service():
+    return await MyModel.objects.all()
 
 
-def get_todo_service(uuid: uuid.UUID):
-    data = get_object_or_404(MyModel, uuid=uuid)
+async def get_todo_service(uuid: uuid.UUID):
+    data = await get_object_or_404(MyModel, uuid=uuid)
     return data
 
 
-def create_todo_service(addData):
-    data = MyModel.objects.create(**addData)
+async def create_todo_service(addData):
+    data = await MyModel.objects.create(**addData)
     return {"id": data.id}
 
 
-def update_todo_service(uuid: uuid.UUID, payload):
-    data = get_object_or_404(MyModel, uuid=uuid)
+async def update_todo_service(uuid: uuid.UUID, payload):
+    data = await get_object_or_404(MyModel, uuid=uuid)
     for attr, value in payload.dict().items():
         setattr(data, attr, value)
     data.save()
     return {"success": True}
 
 
-def update_todo_done_service(uuid: uuid.UUID, payload):
-    data = get_object_or_404(MyModel, uuid=uuid)
+async def update_todo_done_service(uuid: uuid.UUID, payload):
+    data = await get_object_or_404(MyModel, uuid=uuid)
     for attr, value in payload.dict().items():
         if attr == "done":
             setattr(data, attr, value)
@@ -36,7 +36,7 @@ def update_todo_done_service(uuid: uuid.UUID, payload):
     return {"success": True}
 
 
-def delete_todo_service(uuid: uuid.UUID):
-    data = get_object_or_404(MyModel, uuid=uuid)
+async def delete_todo_service(uuid: uuid.UUID):
+    data = await get_object_or_404(MyModel, uuid=uuid)
     data.delete()
     return {"success": True}
