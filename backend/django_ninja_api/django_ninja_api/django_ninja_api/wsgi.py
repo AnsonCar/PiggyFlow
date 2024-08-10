2f5011b790130eabd5fe0792289d5f554d499f97
AnsonCar <anson1tsang@gmail.com>
2024-08-10 20:38:13 +0800

    name

--- backend/django_ninja_api/django_ninja_api/django_ninja_api/wsgi.py
+++ backend/django_ninja_api/django_ninja_api/django_ninja_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for db_api_ninja project.
+WSGI config for django_ninja_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'db_api_ninja.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_ninja_api.settings')
 
 application = get_wsgi_application()
