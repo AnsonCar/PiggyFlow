92d63d3c9fa738c065f445e9083158d73cbfbaba
AnsonCar <anson1tsang@gmail.com>
2024-08-20 08:22:22 +0800

    update download function

--- backend/django-ninja-api/django-ninja-api/api/utils/download.py
+++ backend/django-ninja-api/django-ninja-api/api/utils/download.py
@@ -1,20 +1,47 @@
 import csv
 from django.http import HttpResponse
+from asgiref.sync import sync_to_async
 
-def download_csv(MyModel, file_name:str):
+
+async def download_csv(MyModel, file_name: str):
     response = HttpResponse(content_type="text/csv")
     response["Content-Disposition"] = f'attachment; filename="{file_name}.csv"'
     writer = csv.writer(response)
-    # data column
+
+    # data columns
     model_fields = MyModel._meta.fields
-    field_names = [field.name for field in model_fields]
-    del field_names[field_names.index("id")]
-    del field_names[field_names.index("uuid")]
-    del field_names[field_names.index("user_uuid")]
+    field_names = [field.name for field in model_fields if field.name not in [
+        "id", "uuid", "user_uuid"]]
+
     writer.writerow(field_names)
-    # data rows
-    data = MyModel.objects.all()
+
+    # fetch data asynchronously
+    data = await sync_to_async(list)(MyModel.objects.all())
+
     for item in data:
         row = [str(getattr(item, field)) for field in field_names]
         writer.writerow(row)
+
     return response
+
+# async def download_csv(MyModel, file_name: str):
+#     response = HttpResponse(content_type="text/csv")
+#     response["Content-Disposition"] = f'attachment; filename="{file_name}.csv"'
+#     writer = csv.writer(response)
+
+#     # data column
+#     model_fields = MyModel._meta.fields
+#     field_names = [field.name for field in model_fields]
+
+#     del field_names[field_names.index("id")]
+#     del field_names[field_names.index("uuid")]
+#     del field_names[field_names.index("user_uuid")]
+
+#     writer.writerow(field_names)
+
+#     # data rows
+#     data = [data async for data in MyModel.objects.all()]
+#     for item in data:
+#         row = [str(getattr(item, field)) for field in field_names]
+#         writer.writerow(row)
+#     return response
