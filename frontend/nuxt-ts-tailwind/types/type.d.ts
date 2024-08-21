4d7a158c61044aad10aa71f6e9c2f83ed47a2ebd
AnsonCar <anson1tsang@gmail.com>
2024-08-21 22:57:34 +0800

    add type

--- frontend/nuxt-ts-tailwind/types/type.d.ts
+++ frontend/nuxt-ts-tailwind/types/type.d.ts
@@ -1,10 +1,23 @@
 type TTPSidebar = {
-    text? : string
-    items : TTPSidebarItem[]
+    text?: string
+    items: TTPSidebarItem[]
 }
 
 type TTPSidebarItem = {
     text: string
     link: string
     items?: TTTPSidebar[]
+}
+type TTableFormData = {
+    [key: string]: {
+        uuid: string, user_uuid: string, datetime: string, label: string, price: number
+    }[]
+}
+
+type TTabeData = {
+    date: string,
+    total: number,
+    item: {
+        datetime: string, label: string, price: number, uuid: string
+    }[]
 }
