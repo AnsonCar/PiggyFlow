38cee3a72c28c956e10c5dfb2c5361e37860d919
AnsonCar <anson1tsang@gmail.com>
2024-09-14 14:27:02 +0800

    feat: script

---
+++ k8s/k8s.sh
@@ -0,0 +1,24 @@
+kubectl apply -f ./k8s/k8s.yaml -n piggyflow
+
+kubectl create secret generic piggyflow-env-production --from-env-file=.env.production
+kubectl create secret generic piggyflow-env-production --from-env-file=backend/django-ninja-api/env/.env.production
+kubectl get secret piggyflow-env-production
+
+# backend/django-ninja-api/env/.env.production
+
+minikube image load piggyflow-django-ninja-api:latest
+
+kubectl apply -f ./k8s/backend-deployment.yml -n piggyflow
+kubectl get pods -n piggyflow
+
+kubectl delete -f ./k8s/backend-deployment.yml -n piggyflow
+
+kubectl logs piggyflow-backend-deployment-fdfbcbd69-477jq -n piggyflow
+
+kubectl describe pod piggyflow-backend-deployment-fdfbcbd69-477jq -n piggyflow
+
+minikube service list
+
+# piggyflow-backend-deployment-85b4dd6db-wcg62
+
+kubectl exec piggyflow-backend-deployment-85b4dd6db-8zwr7 -- printenv
