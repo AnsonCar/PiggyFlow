aa756e7d8d3bf8a1f767c320521994d61334ad4b
AnsonCar <anson1tsang@gmail.com>
2024-10-23 09:13:36 +0800

    feat: change url path

--- backend/django-ninja-api/django-ninja-api/django-ninja-api/urls.py
+++ backend/django-ninja-api/django-ninja-api/django-ninja-api/urls.py
@@ -1,25 +1,30 @@
-"""
-URL configuration for django-ninja-api project.
+import os
+import orjson
+from ninja_extra import NinjaExtraAPI
+from ninja.parser import Parser
+from ninja_jwt.controller import NinjaJWTDefaultController
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
+class ORJSONParser(Parser):
+    def parse_body(self, request):
+        return orjson.loads(request.body)
+
+
+MODE = os.getenv("DJANGO_DEPLOY", "Null")
+NAME = f"DataBase NinjaExtra API | {MODE.capitalize()}"
+
+api = NinjaExtraAPI(app_name=NAME, title=NAME, parser=ORJSONParser())
+api.register_controllers(NinjaJWTDefaultController)
+
+from api.transaction.router import router as transactionRouter
+from api.todo.router import router as todoRouter
+
+api.add_router("/transaction", transactionRouter)
+api.add_router("/todo", todoRouter)
 
 from django.urls import path
 
-from api.views import api
-
 urlpatterns = [
-    path('api/', api.urls),
+    path("api/", api.urls),
     # path('admin/', admin.site.urls),
 ]
