034314bca28d27f5204e8b19afecc12c6d61eb25
AnsonCar <anson1tsang@gmail.com>
2024-10-23 23:03:51 +0800

    update

--- backend/django-ninja-api/django-ninja-api/django-ninja-api/urls.py
+++ backend/django-ninja-api/django-ninja-api/django-ninja-api/urls.py
@@ -1,33 +1,22 @@
 import os
-import orjson
+from django.urls import path
+from .parser import ORJSONParser
 from ninja_extra import NinjaExtraAPI
-from ninja.parser import Parser
 from ninja_jwt.controller import NinjaJWTDefaultController
-
-
-# %%
-class ORJSONParser(Parser):
-    def parse_body(self, request):
-        return orjson.loads(request.body)
-
+from api.transaction.router import router as transactionRouter
+from api.todo.router import router as todoRouter
+from ansc_auth.views import router as userRouter, routerGroup as userGroupRouter
 
 MODE = os.getenv("DJANGO_DEPLOY", "Null")
 NAME = f"DataBase NinjaExtra API | {MODE.capitalize()}"
 
 api = NinjaExtraAPI(app_name=NAME, title=NAME, parser=ORJSONParser())
 api.register_controllers(NinjaJWTDefaultController)
-
-# %%
-from api.transaction.router import router as transactionRouter
-from api.todo.router import router as todoRouter
-from ansc_auth.views import router as userRouter, routerGroup as userGroupRouter
-
-api.add_router("/transaction", transactionRouter)
-api.add_router("/todo", todoRouter)
 api.add_router("/user", userRouter)
 api.add_router("/group", userGroupRouter)
+api.add_router("/transaction", transactionRouter)
+api.add_router("/todo", todoRouter)
 
-from django.urls import path
 
 urlpatterns = [
     path("api/", api.urls),
