3b7f258187c57ec1a38a3c335363b2b34d02f88c
AnsonCar <anson1tsang@gmail.com>
2024-10-26 22:37:18 +0800

    fea: env

--- frontend/nuxt-ts-tailwind/nuxt.config.ts
+++ frontend/nuxt-ts-tailwind/nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      titleTemplate: 'PiggyFlow | %s',
+      titleTemplate: `%s | ${process.env.PROJECT_NAME}`,
       // title: 'PiggyFlow',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
@@ -26,7 +26,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      DB_API_BASE_URL: process.env.DB_API_BASE_URL,
+      PROJECT_NAME: process.env.PROJECT_NAME,
+      DJANGO_URL: process.env.DJANGO_URL,
     },
   },
 
