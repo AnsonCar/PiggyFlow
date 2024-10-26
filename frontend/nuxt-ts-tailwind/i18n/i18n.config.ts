59ef5505c4defe379b0bbc13d022067815e122ca
AnsonCar <anson1tsang@gmail.com>
2024-10-26 22:22:29 +0800

    feat: update i18n

--- frontend/nuxt-ts-tailwind/i18n/i18n.config.ts
+++ frontend/nuxt-ts-tailwind/i18n/i18n.config.ts
@@ -1,6 +1,6 @@
-import { basis as zh } from './zh_tw/index'
+import { basis as zh_tw } from './zh-TW/index'
 import { basis as en } from './en/index'
-import { basis as zh_cn } from './zh_cn/index'
+import { basis as zh_cn } from './zh-CN/index'
 
 export default defineI18nConfig(() => ({
     legacy: false,
@@ -8,12 +8,12 @@ export default defineI18nConfig(() => ({
     fallbackLocale: 'zh',
     locales: [
         { code: 'en', iso: 'en-US' },
-        { code: 'zh', iso: 'zh-TW' },
+        { code: 'zh-TW', iso: 'zh-TW' },
         { code: 'zh-CN', iso: 'zh-CN' }
     ],
     messages: {
         en: en,
-        zh: zh,
+        zh: zh_tw,
         'zh-CN': zh_cn
     }
 }))
