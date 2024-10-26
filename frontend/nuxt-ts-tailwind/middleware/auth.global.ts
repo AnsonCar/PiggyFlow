dfc4ac6195306b635d4798267b2e3579ecef76bf
AnsonCar <anson1tsang@gmail.com>
2024-10-26 21:57:50 +0800

    change name

--- frontend/nuxt-ts-tailwind/middleware/auth.global.ts
+++ frontend/nuxt-ts-tailwind/middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  let user = useState('auth');
-  if (user.value && to.path === '/login') return navigateTo('/');
-  if (!user.value && to.path !== '/login') return navigateTo('/login');
+  let auth = useState('auth');
+  if (auth.value && to.path === '/login') return navigateTo('/');
+  if (!auth.value && to.path !== '/login') return navigateTo('/login');
   if (to.path === '/') return navigateTo('/account');
 });
