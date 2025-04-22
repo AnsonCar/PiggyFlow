b93c554d4a72104f092e5b8a89ba27d5a33701c4
AnsonCar <anson1tsang@gmail.com>
2025-04-22 22:33:40 +0800

    feat(frontend): add page transition animation
    - Implement a basic transition effect for page changes
    - Add transition component to RouterView
    - Include CSS for transition effects (currently commented out)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="size-full font-sans">
-    <RouterView />
+    <router-view v-slot="{ Component }">
+      <transition>
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+/* .v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+} */
+</style>
