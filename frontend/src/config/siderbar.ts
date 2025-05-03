2f84bab12569fe2ba7d29bc6c980f83aef4d0dbc
AnsonCar <anson1tsang@gmail.com>
2025-05-03 13:29:47 +0800

    feat(frontend):

---
+++ frontend/src/config/siderbar.ts
@@ -0,0 +1,23 @@
+export const siderItems = [
+  {
+    items: [
+      { text: '儀表板', name: 'home' },
+      { text: '測試場', name: 'test' },
+    ],
+  },
+  // {
+  //   text: '工具箱',
+  //   items: [
+  //     { text: '要事', name: 'todo' },
+  //     { text: '記帳', name: 'account' },
+  //     { text: 'QRCode', name: 'qrcode' },
+  //   ],
+  // },
+  // {
+  //   text: '身份管理',
+  //   items: [
+  //     { text: '用戶', name: 'user' },
+  //     { text: '組別', name: 'group' },
+  //   ],
+  // },
+];
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
-  <Alert />
   <main class="bg-base-300">
-    <TPSidebar button-label="main" :items="siderbaritems">
+    <Alert />
+    <TPSidebar button-label="main" :items="siderItems">
       <TPTopbar class="lg:hidden" />
       <main class="h-[calc(100%-64px)] overflow-y-scroll p-4 pt-0 lg:h-full lg:pt-4">
         <router-view />
@@ -14,44 +14,5 @@
 import TPSidebar from '@/components/tp/sidebar/tp-sidebar.vue';
 import TPTopbar from '@/components/tp/topbar/tp-topbar.vue';
 import Alert from '@/components/tp/tp-alert.vue';
-
-const siderbaritems = [
-  {
-    items: [
-      { text: '儀表板', name: 'home' },
-      { text: '測試場', name: 'test' },
-    ],
-  },
-  // {
-  //   text: '工具箱',
-  //   items: [
-  //     { text: '要事', name: 'todo' },
-  //     { text: '記帳', name: 'account' },
-  //     { text: 'QRCode', name: 'qrcode' },
-  //   ],
-  // },
-  // {
-  //   text: '身份管理',
-  //   items: [
-  //     { text: '用戶', name: 'user' },
-  //     { text: '組別', name: 'group' },
-  //   ],
-  // },
-];
+import { siderItems } from '@/config/siderbar';
 </script>
-
-<style>
-.page-enter-active {
-  transition: all 0.1s ease-out;
-}
-
-.page-leave-active {
-  transition: all 0.1s ease-in;
-}
-
-.page-enter-from,
-.page-leave-to {
-  opacity: 0.2;
-  filter: blur(1rem);
-}
-</style>
