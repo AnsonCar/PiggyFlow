afba1d927fc59774e6cb18b8e772681403e6322a
AnsonCar <anson1tsang@gmail.com>
2025-06-09 23:21:22 +0800

    add vitepress-plugin-llms

--- _document/vitepress/.vitepress/config.mjs
+++ _document/vitepress/.vitepress/config.mjs
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
+import llmstxt from "vitepress-plugin-llms";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  srcDir: './src',
-  base: '/PiggyFlow/',
+  vite: {
+    plugins: [llmstxt()],
+  },
+  srcDir: "./src",
+  base: "/PiggyFlow/",
   title: "Piggy Flow",
   description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: '路線圖', link: '/roadmap'},
-    ],
+    nav: [{ text: "路線圖", link: "/roadmap" }],
 
     sidebar: {
-      '/development' : [
-        { text: 'overview', link: '/development/overview'},
-      ]
+      "/development": [{ text: "overview", link: "/development/overview" }],
     },
 
     footer: {
-      message: 'Design By AnsonCar',
-      copyright: 'Copyright © 2024 Cetopus | STAR | Carhyme'
+      message: "Design By AnsonCar",
+      copyright: "Copyright © 2024 Cetopus | STAR | Carhyme",
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/AnsonCar/PiggyFlow' }
-    ]
+      { icon: "github", link: "https://github.com/AnsonCar/PiggyFlow" },
+    ],
   },
-})
+});
