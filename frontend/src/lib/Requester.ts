e7c20fbcd7f617540f83387f3c2f559a380d8df5
AnsonCar <anson1tsang@gmail.com>
2025-06-15 01:21:04 +0800

    refactor(frontend): improve error handling in Requester
    - Extract server error response to a constant
    - Optimize error message handling in api function
    - Simplify error handling in apiFile function

--- frontend/src/lib/Requester.ts
+++ frontend/src/lib/Requester.ts
@@ -1,43 +1,43 @@
 import { useAlertStore } from '@/stores/alertStore';
 import { useAuthStore } from '@/stores/authStore';
 
+const serverError = {
+  error: 'An error occurred.',
+  code: 500,
+  message: 'Server Error.',
+};
+
 export async function api(url: string, options: RequestInit | undefined) {
   // 取得 url 資源 和 選項
   const BasisApiURL = 'http://localhost:8000';
   const alertStore = useAlertStore();
   try {
-    // 請求
-    const res = await fetch(`${BasisApiURL}${url}`, options);
     const authStore = useAuthStore();
-
     if (!url.startsWith('/api/token')) {
       authStore.reFresh();
     }
-
-    if (res.status !== 200) {
-      alertStore.addItem({
-        message: res.statusText,
-        level: 'error',
-      });
-    }
-
+    // 請求
+    const res = await fetch(`${BasisApiURL}${url}`, options);
     const data = await res.json();
-
-    if ('detail' in data) {
-      alertStore.addItem({
-        message: data.detail,
-        level: 'error',
-      });
+    if (!url.startsWith('/api/token')) {
+      let message = '';
+      if (res.status !== 200) {
+        message = res.statusText;
+      }
+      if ('detail' in data) {
+        message = data.detail;
+      }
+      if (message.length > 0) {
+        alertStore.addItem({
+          message: message,
+          level: 'error',
+        });
+      }
     }
-
     return data;
   } catch (error) {
     // 如果 請求失敗（Server 死咗 ）
-    return {
-      error: 'An error occurred.',
-      code: 500,
-      message: 'Server Error.',
-    };
+    return serverError;
   }
 }
 
@@ -51,11 +51,7 @@ export async function apiFile(url: string, options: RequestInit | undefined) {
     return blob;
   } catch (error) {
     // 如果 請求失敗（Server 死咗 ）
-    return {
-      error: 'An error occurred.',
-      code: 500,
-      message: 'Server Error.',
-    };
+    return serverError;
   }
 }
 
